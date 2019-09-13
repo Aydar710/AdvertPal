980434449b0e0019460d8e4041553399f88d8128
Aydar <aydar710@gmail.com>
2019-09-13 12:46:03 +0300

    add item_work

---
+++ app/src/main/java/com/example/advertpal/features/works/WorksActivity.kt
@@ -0,0 +1,13 @@
+package com.example.advertpal.features.works
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.advertpal.R
+
+class WorksActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_works)
+    }
+}
